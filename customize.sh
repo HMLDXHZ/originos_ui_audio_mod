703a075660c2ebede919c522a77bf7f3eae4447e
DRAGONqwq <47932517+HMLDXHZ@users.noreply.github.com>
2026-01-16 16:22:52 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,4 +1,4 @@
   ui_print "$pounds"
-  ui_print " 作者HMLDXHZ 酷安@2700890
-  ui_print " 锁屏音效，解锁音效，电池充满音效已修改，重启生效！
+  ui_print "- 作者HMLDXHZ 酷安@2700890"
+  ui_print "- 锁屏音效，解锁音效，电池充满音效已修改，重启生效！"
   ui_print "$pounds"
